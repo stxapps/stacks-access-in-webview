7549fb34f39bf3aefbb2d0f1f223d43b389d9cb9
wit <witwitchayakarn@gmail.com>
2024-02-19 15:04:08 +0700

    Refactor HR_HUB_URL and SD_HUB_URL

--- sign-in/src/types/const.js
+++ sign-in/src/types/const.js
@@ -1,1 +1,4 @@
+export const HR_HUB_URL = 'https://hub.hiro.so';
+export const SD_HUB_URL = 'https://hub.stacksdrive.com';
+
 export const HASH_SUPPORT = '#support';
--- sign-up/src/types/const.js
+++ sign-up/src/types/const.js
@@ -1,3 +1,6 @@
+export const HR_HUB_URL = 'https://hub.hiro.so';
+export const SD_HUB_URL = 'https://hub.stacksdrive.com';
+
 export const HASH_TERMS = '#terms';
 export const HASH_PRIVACY = '#privacy';
 export const HASH_SUPPORT = '#support';
