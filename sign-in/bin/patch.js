f712b4313fab0f82db6b79aa229f40c875c871fa
wit <witwitchayakarn@gmail.com>
2024-08-20 15:03:05 +0700

    Fix root is not a DOM element and missing crypto.pbkdf2

--- sign-in/bin/patch.js
+++ sign-in/bin/patch.js
@@ -36,4 +36,33 @@ const patchFetch = () => {
   );
 };
 
+const patchCryptoUtils = () => {
+  let match = "            const nodeCrypto = require('crypto');";
+  let repmt = "            const nodeCrypto = require('crypto-browserify');";
+  replaceMatchedLine(
+    'node_modules/@stacks/encryption/dist/cryptoUtils.js',
+    [{ match, repmt }],
+  );
+  replaceMatchedLine(
+    'node_modules/@stacks/encryption/dist/esm/cryptoUtils.js',
+    [{ match, repmt }],
+  );
+};
+
+const patchInlineChunkHtmlPlugin = () => {
+  // github.com/facebook/create-react-app/issues/10752
+  // github.com/facebook/create-react-app/pull/10753/files
+
+  let match1 = '        assets.headTags = assets.headTags.map(tagFunction);';
+  let repmt1 = '        assets.bodyTags = [...assets.headTags.map(tagFunction), ...assets.bodyTags.map(tagFunction)];';
+  let match2 = '        assets.bodyTags = assets.bodyTags.map(tagFunction);';
+  let repmt2 = '        assets.headTags = [];';
+  replaceMatchedLine(
+    'node_modules/react-dev-utils/InlineChunkHtmlPlugin.js',
+    [{ match: match1, repmt: repmt1 }, { match: match2, repmt: repmt2 }],
+  );
+};
+
 patchFetch();
+patchCryptoUtils();
+patchInlineChunkHtmlPlugin();
